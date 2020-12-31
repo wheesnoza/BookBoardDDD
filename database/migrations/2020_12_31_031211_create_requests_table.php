e202cb78821704122ac29c61a9a618dd7bb601c2
wheesnoza <h.watanabe2529@gmail.com>
2020-12-31 12:17:30 +0900

    add first migrations and models

---
+++ database/migrations/2020_12_31_031211_create_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('book_id');
+            $table->bigInteger('employee_id');
+            $table->timestamp('requested_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
